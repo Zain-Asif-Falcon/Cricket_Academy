240a522edcdd464927ef0752f99c378953497b9a
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:59:06 +0500

    Create IVendorRepository.cs

---
+++ AQCricketAcademy/Application/Business/Interfaces /IVendorRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Business.GenericInterfaces;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Business.Interfaces
+{
+    public interface IVendorRepository : IGenericInterface<Vendor>
+    {
+        IEnumerable<Vendor> GetActiveItemVendors();
+        bool CheckDuplicateVendor(string VendorName, string VendorAddress, int id);
+        string VendorCode();
+    }
+}
