3d4f946f0ccb14a824e3c817f2f64897f9d7effa
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:21:34 +0500

    Create VendorPayment.cs

---
+++ AQCricketAcademy/Domain/Entities /VendorPayment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class VendorPayment : GenericModel
+    {
+        [ForeignKey("VendorReceipt")]
+        public int VendorReceipt_id { get; set; }
+        public virtual VendorReceipt VendorReceipt { get; set; }      
+        public DateTime PaymentDate { get; set; }
+    }
+}
