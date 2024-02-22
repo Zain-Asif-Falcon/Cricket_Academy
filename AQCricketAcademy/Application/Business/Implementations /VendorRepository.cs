017071a5d84579503633b97d6cfe0cacbc1c68f6
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:56:20 +0500

    Create VendorRepository.cs

---
+++ AQCricketAcademy/Application/Business/Implementations /VendorRepository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Business.GenericInterfaces;
+using Application.Business.Interfaces;
+using Domain.Entities;
+using Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Business.Implementations
+{
+    public class VendorRepository : GenericInterfaceImplementation<Vendor>, IVendorRepository
+    {
+        private readonly CandidateDataContext _Context;
+        public VendorRepository(CandidateDataContext Context) : base(Context)
+        {
+            _Context = Context;
+        }            
+        public string VendorCode()
+        {           
+            int count = 0;
+
+            if (string.IsNullOrWhiteSpace(VC))
+            {
+                VC = "V" + 1.ToString().PadLeft(4, '0');
+            }
+            else
+            {
+                count = Convert.ToInt32(VC.Substring(1)) + 1;
+                VC = "V" + count.ToString().PadLeft(4, '0');
+            }
+            return VC;
+        }
+    }
+}
