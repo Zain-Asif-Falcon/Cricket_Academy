b96db4caf46b3e00e7e456b8642a749c9e9ea42b
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:18:18 +0500

    Create Item.cs

---
+++ AQCricketAcademy/Domain/Entities /Item.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Item : GenericModel
+    {
+        public string ItemName { get; set; }
+        public string ItemCode { get; set; }
+        [ForeignKey("ItemCategory")]
+        public int? ItemCategoryId { get; set; }     
+        public decimal Cost { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; } 
+        public IEnumerable<ItemPurchaseGoods> ItemPurchaseGoods { get; set; }   
+
+    }
+}
