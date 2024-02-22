379376c96f0bd600dd87b1febc26cd0223c1c093
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 13:16:41 +0500

    Create PackagesController.cs

---
+++ AQCricketAcademy/AQCricket_API/Controllers /PackagesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain.Entities;
+using Infrastructure;
+using Application.Business.UnitOfWork;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Domain.Helper;
+
+namespace AQAcademy_API.Controllers
+{
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public PackagesController(IUnitOfWork UOW)
+        {
+            // _context = context;
+            _UnitOfWork = UOW;
+
+        }
+
+        // GET: api/Packages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Packages>>> GetPackages()
+        {
+            return _UnitOfWork.Package.GetActivePackages().ToList();
+        }
+
+        // GET: api/Packages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Packages>> GetPackages(int id)
+        {
+            var package = _UnitOfWork.Package.FindById(id);
+
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            return package;
+        }    
+    }
+}
