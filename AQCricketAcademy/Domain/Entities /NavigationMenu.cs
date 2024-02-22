832c8ffbbd914fe1ce58dd0a0adb6e51f02987c0
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:24:08 +0500

    Create NavigationMenu.cs

---
+++ AQCricketAcademy/Domain/Entities /NavigationMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+	[Table(name: "AspNetNavigationMenu")]
+	public class NavigationMenu
+    {
+		[Key]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string ActionName { get; set; }
+
+		public bool IsExternal { get; set; }		
+
+		public bool Visible { get; set; }
+	}
+}
