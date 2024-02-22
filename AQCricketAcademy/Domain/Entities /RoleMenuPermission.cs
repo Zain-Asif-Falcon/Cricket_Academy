dae3feab49c0b4af4c4c3058dbd5767bffd5ec5d
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:27:43 +0500

    Create RoleMenuPermission.cs

---
+++ AQCricketAcademy/Domain/Entities /RoleMenuPermission.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    [Table(name: "AspNetRoleMenuPermission")]
+    public class RoleMenuPermission
+    {
+        [Key]
+        public int RoleMenuPermissionId { get; set; }
+        public string RoleId { get; set; }    
+    }
+}
