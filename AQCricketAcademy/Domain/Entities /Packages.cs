788b30c0ca880c6258d05c0256be3ea63c3eba0b
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:25:04 +0500

    Create Packages.cs

---
+++ AQCricketAcademy/Domain/Entities /Packages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Packages : GenericModel
+    {
+        [Display(Name = "Package Name")]
+        public string PackageName { get; set; }
+        [Display(Name = "No Of Classes")]
+        public int NumberOfClasses { get; set; }       
+        public int TotalPrice { get; set; }
+    }
+}
