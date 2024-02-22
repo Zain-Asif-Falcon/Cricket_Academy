e3e2f7576c6855f5ee9f4e0a9e948cb12c80a149
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:19:30 +0500

    Create ItemSaleOders.cs

---
+++ AQCricketAcademy/Domain/Entities /ItemSaleOders.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class ItemSaleOders : GenericModel
+    {
+        public DateTime SaleOrderDate { get; set; }
+        public string SaleOrderCode { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TotalAmount { get; set; }     
+        [ForeignKey("PlayerMemberShip")]
+        public int? PlayerId { get; set; }      
+        public DateTime? DueDate { get; set; }
+        public List<CustomerReceipt> CustomerReceipts { get; set; }
+        public virtual PlayerMemberShip PlayerMemberShip { get; set; }
+    }
+    }
+}
