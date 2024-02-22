bb15e8c554c37b756b1c5efb7d3e8661680ad733
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:26:31 +0500

    Create CustomerPayment.cs

---
+++ AQCricketAcademy/Domain/Entities /CustomerPayment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class CustomerPayment:GenericModel
+    {
+        [ForeignKey("CustomerReceipt")]
+        public int CustomerReceipt_id { get; set; }
+        public virtual CustomerReceipt CustomerReceipt { get; set; }       
+        [Required]
+        public DateTime PaymentDate { get; set; }
+    }
+}
