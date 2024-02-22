5ab94a505826eee6ceae00c5fdc91cb208c6b955
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 13:18:05 +0500

    Create UserRolesController.cs

---
+++ AQCricketAcademy/AQCricket_API/Controllers /UserRolesController.cs
@@ -0,0 +1,35 @@
+﻿using Application.Business.UnitOfWork;
+using Domain.DTO;
+using Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AQAcademy_API.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserRolesController : ControllerBase
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public UserRolesController(IUnitOfWork UOW)
+        {
+            _UnitOfWork = UOW;
+        }       
+        [Route("GetRoleMenuPermissions")]
+        [HttpPost]
+        public List<RoleMenuPermission> GetRoleMenuPermissions(IEnumerable<string> userRoles)
+        {
+            var roleMenus = _UnitOfWork.DataAccess.GetRoleMenuPermissions(userRoles);
+            return roleMenus;
+        }
+    }
+}
