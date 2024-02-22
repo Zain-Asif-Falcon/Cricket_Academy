d40cb75df95b94f72373e92ec46148d4a6c85577
Zain Asif <93377394+Zain-Asif-Falcon@users.noreply.github.com>
2024-02-22 12:34:39 +0500

    Create ItemSaleOrderDTO.cs

---
+++ AQCricketAcademy/Domain/DTO /ItemSaleOrderDTO.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class ItemSaleOrderDTO
+    {
+        public int OrderId { get; set; }
+        public string SaleOrderDate { get; set; }       
+        public bool IsReceipt { get; set; }
+        public virtual ICollection<ItemSaleGoods> ItemSaleGoods { get; set; }
+    }
+}
